bb6e873bdcc9280323a457bf1ac3cd2e101cf16a
Homero Batista Kzam <homero@parkcode.mobi>
2019-02-24 10:22:15 -0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import { Toolbar, Drawer, TreeView } from 'devextreme-react';
+
+const Dashboard = () => {
+  return(
+    <>
+      Dashboard!!!
+    </>
+  )
+}
+
+const About = () => {
+  return(
+    <>
+      ABOUT
+    </>
+  )
+}
+
+const navigation = [
+  { id: 1, text: 'Dashboard', icon: 'chart' },
+  { id: 2, text: 'About', icon: 'about' },
+];
+
+const MainMenu = (props) => {
+  return(
+    <>
+      <TreeView dataSource={navigation} />
+    </>
+  )
+}
+
+const App = () => {
+  const [opened, setOpened] = useState(true);
+
+  const toolbarItems = [{
+    widget: 'dxButton',
+    location: 'before',
+    options: {
+      icon: 'menu',
+      onClick: () => setOpened(!opened)
+    }
+  }];
+
+  return (
+    <div className="App">
+      <Toolbar items={toolbarItems}></Toolbar>
+      <Drawer opened={opened} openedStateMode={'shrink'} position={'left'} component={MainMenu} closeOnOutsideClick={true} >
+        <BrowserRouter>
+          <Switch>
+            <Route exact={true} path="/" component={Dashboard} />
+            <Route path="/dash" component={Dashboard} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </BrowserRouter>
+      </Drawer>
+    </div>
+  );
 }
 
 export default App;
